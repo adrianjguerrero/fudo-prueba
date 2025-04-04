957f3e286e4ac39233c843e5a868377719898e1e
Adrian Guerrero <adrianjguerrero87@gmail.com>
2025-04-04 18:32:22 -0400

    created auth endpoint

--- app.rb
+++ app.rb
@@ -1,5 +1,32 @@
 require 'sinatra'
+require 'json'
+require 'securerandom'
 
-get '/' do
-  'Hello world!'
+
+
+# simulacion de base de datos
+USERS = { "user" => "clave123" }
+PRODUCTS = {}
+
+
+post '/auth' do
+  content_type :json
+
+  begin
+    params_request = JSON.parse(request.body.read)
+    user = params_request["usuario"]
+    password = params_request["contrasena"]
+  rescue  Exception => e
+    puts e
+    halt 500, { error: "Fallo procesando tu petición" }.to_json
+  end
+
+  
+
+  if USERS[user] && USERS[user] == password
+    token = SecureRandom.hex(16)
+    { token: token }.to_json
+  else
+    halt 404, { error: "Fallo iniciando sesión" }.to_json
+  end
 end
